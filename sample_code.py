35ade994bce3f463eb4a9f8dfb3ccc715de9e6e7
nayansf <150217265+nayansf@users.noreply.github.com>
2023-11-15 15:02:51 +0530

    Update sample_code.py

--- sample_code.py
+++ sample_code.py
@@ -1,10 +1,14 @@
-# Example vulnerable code with a security flaw
+# Example vulnerable code with a security flaw (including a potential secret)
 import os
 
 def execute_command(command):
     os.system(command)
 
+# Simulated secret (for demonstration purposes, replace this with an actual secret)
+api_key = "my_secret_api_key"
+
 input_command = input("Enter a command to execute: ")
 execute_command(input_command)
 
-password="testmescanner"
+# Using the simulated secret (in a real scenario, avoid hardcoding secrets in code)
+print("API Key:", api_key)
